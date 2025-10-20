e9a967e915fe10abadf0cd616d89f9477a81bc8a
Duy-Phan-prg <phanduy.prg@gmail.com>
2025-10-21 00:32:59 +0700

    Update layout.tsx

--- src/app/menu/layout.tsx
+++ src/app/menu/layout.tsx
@@ -8,7 +8,7 @@ export default function MenuLayout({ children }: { children: React.ReactNode }) 
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
 
-      <main className="flex-1 container mx-auto px-4 py-6">
+      <main className="flex-1 container mx-auto px-4 py-6 pt-20">
         {children}
       </main>
 
